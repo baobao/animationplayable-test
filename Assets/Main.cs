fe6d04ab116954c4089a878e3f168c77d68f93c2
baobao <shunsuke.ohba@gmail.com>
2018-06-13 10:25:15 +0900

    add initial

---
+++ Assets/Main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(Animator))]
+public class Main : MonoBehaviour
+{
+    [SerializeField]
+    private AnimationClip _clip;
+
+    [SerializeField]
+    private ParticleSystem _p;
+
+    void Start()
+    {
+        var g = PlayableGraph.Create();
+        var clipPlayable = AnimationClipPlayable.Create(g, _clip);
+        var output = AnimationPlayableOutput.Create(g, "output", GetComponent<Animator>());
+        output.SetSourcePlayable(clipPlayable);
+        g.Play();
+        _p.Play();
+    }
+}
